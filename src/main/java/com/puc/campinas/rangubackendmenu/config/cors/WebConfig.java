be74ff98c51177738009dbe3d7103bde7c876481
Gian Costa <rafa_gian2011@hotmail.com>
2021-10-25 22:30:43 -0300

    swagger-fix

--- src/main/java/com/puc/campinas/rangubackendmenu/config/cors/WebConfig.java
+++ src/main/java/com/puc/campinas/rangubackendmenu/config/cors/WebConfig.java
@@ -3,7 +3,9 @@ package com.puc.campinas.rangubackendmenu.config.cors;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @EnableWebMvc
@@ -12,6 +14,12 @@ public class WebConfig implements WebMvcConfigurer {
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**");
+  }
+
+  @Override
+  public void configureViewResolvers(ViewResolverRegistry registry) {
+    registry.viewResolver(new InternalResourceViewResolver());
+    WebMvcConfigurer.super.configureViewResolvers(registry);
   }
 
 }
